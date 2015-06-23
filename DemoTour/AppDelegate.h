1a6dd3651e42b9e324ab31bd3220f30ce23880f4
amitc3353 <amitc3353@gmail.com>
2015-06-22 21:47:41 -0400

    Initial commit for local git

---
+++ DemoTour/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoTour
+//
+//  Created by Amit Chandel on 6/22/15.
+//  Copyright (c) 2015 Amit Chandel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
